a801e061d2bedb71dc794904237e4397ce774f40
jonathan <jonathan.lapiquonne@gmail.com>
2022-04-29 19:49:52 -0400

    feat(docs): setup project

---
+++ apps/docs/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+import { Button } from "mwaka";
+
+const App: Component = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
