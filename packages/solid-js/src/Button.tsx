c6b901b0a4f51eec854adddc44cdf23ebd0bd0a7
snapiz <jonathan.lapiquonne@gmail.com>
2022-05-04 01:52:37 +0200

    fix: wrong local package deps

--- packages/solid-js/src/Button.tsx
+++ packages/solid-js/src/Button.tsx
@@ -4,7 +4,7 @@ import { useCount } from "pikav";
 const Button: Component = () => {
   const [count, { setCount }] = useCount();
 
-  return <button onClick={() => setCount(count() + 1)}>Try click {count()}</button>;
+  return <button onClick={() => setCount(count() + 1)}>Click me {count()}</button>;
 };
 
 export default Button;
