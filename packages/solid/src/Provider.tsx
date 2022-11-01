6418cbbb4aa2ea9881fd23e2aaa48948ca9909e6
snapiz <jonathan.lapiquonne@gmail.com>
2022-11-01 15:46:20 +0100

    fix: bump solid to 1.6.1

--- packages/solid/src/Provider.tsx
+++ packages/solid/src/Provider.tsx
@@ -1,12 +1,12 @@
 import { Client } from "pikav";
-import { Component } from "solid-js";
+import { ParentComponent } from "solid-js";
 import { Context } from "./hooks";
 
 export interface ProviderProps {
   client: Client | Client[];
 }
 
-const Provider: Component<ProviderProps> = (props) => {
+const Provider: ParentComponent<ProviderProps> = (props) => {
   const clients = !Array.isArray(props.client)
     ? [props.client as Client]
     : (props.client as Client[]);
